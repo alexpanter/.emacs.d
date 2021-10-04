47c9e9e9fc6ada68fb6df28224f518350d66ad38
alexpanter <alex_c007@hotmail.com>
2021-10-04 11:14:24 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,452 @@
+;;;;;;;;;;;;;;
+;; PACKAGES ;;
+;;;;;;;;;;;;;;
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; activate all the packages (in particular autoloads)
+(package-initialize)
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+
+;;;;;;;;;;;;;;;;;;;;;
+;; GLOBAL SETTINGS ;;
+;;;;;;;;;;;;;;;;;;;;;
+(blink-cursor-mode 0)
+(global-undo-tree-mode 1)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(show-paren-mode t)
+(setq inhibit-startup-screen 1)
+;; yes-or-no
+(defalias 'yes-or-no-p 'y-or-n-p)
+;; update any change made on file to the current buffer
+(global-auto-revert-mode)
+;; when this mode is active, marked text will be replaced with newly written
+(delete-selection-mode)
+;; better scrolling through files.
+;; Cursor at the top line and pressing key up won't move the file many, many
+;; lines upwards as is the default.
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position nil)
+;; toggle line highlighting in all buffers
+(global-hl-line-mode)
+;; winner-mode, easy switch between views
+(global-set-key (kbd "C-c <left>") 'winner-undo)
+(global-set-key (kbd "C-c <right>") 'winner-redo)
+(winner-mode t)
+
+
+;;;;;;;;;;;;;;;
+;; HELM-MODE ;;
+;;;;;;;;;;;;;;;
+(require 'helm)
+(require 'helm-config)
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change 'helm-command-prefix-key' once 'helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-config-prefix)
+(global-unset-key (kbd "C-x c"))
+;; rebind tab to run persistent action
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+;; make TAB work in terminal
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+;; list actions using C-z
+(define-key helm-map (kbd "C-z") 'helm-select-action)
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+;;
+(setq
+ ;; open helm buffer inside current window, not occupy whole other window
+ helm-split-window-in-side-p           t
+
+ ;; move to end or beginning of source when reaching top or bottom
+ helm-move-to-line-cycle-in-source     t
+
+ ;; search for library in 'require' and 'declare-function' sexp.
+ helm-ff-search-library-in-sexp        t
+
+ ;; scroll 8 lines other window using M-<next>/M-<prior>
+ helm-scroll-amount                    8
+ helm-ff-file-name-history-use-recentf t
+ )
+;; by enabling auto-resize, Helm will resize its buffer automatically
+;; to fit the number of search candidates.
+(helm-autoresize-mode t)
+;; Command: helm-M-x
+(global-set-key (kbd "M-x") 'helm-M-x)
+;; if this is activated, helm-M-x will fuzzy match candidates
+(setq helm-M-x-fuzzy-match t) ; optional fuzzy match for helm-M-x
+;; enable the helm kill-ring with binding it to M-y
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+;; helm-mini:
+(global-set-key (kbd "C-x b") 'helm-mini)
+;; to enable fuzzy matching with helm-mini, add the following settings:
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+;; helm-find-files - file navigation on steroids!
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; if the point is located on a folder path, C-x C-f will
+;; start in that folder. (Use C-l C-r to navigate directories)
+;; use arrow keys to navigate helm-find-files menu
+(setq helm-ff-lynx-style-map t)
+(helm-mode 1)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; BASIC TEXT EDITING ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; rebind C-a to move to the beginning of text instead of beginning of line
+(defun prelude-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+If ARG is not nil or 1, move forward ARG - 1 lines first. If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+;;
+(global-set-key (kbd "C-a") 'prelude-move-beginning-of-line)
+;;
+;; hover with mouse over a symbol or definition and press
+;; C-S-mouse-1 and see the definition highlighted all through
+;; the file. Really useful.
+(require 'highlight-symbol)
+;;
+(highlight-symbol-nav-mode)
+;;
+(add-hook 'prog-mode-hook (lambda () (highlight-symbol-mode)))
+(add-hook 'org-mode-hook (lambda () (highlight-symbol-mode)))
+;;
+(setq highlight-symbol-idle-delay 0.2
+      highlight-symbol-on-navigation-p t)
+;;
+(global-set-key [(control shift mouse-1)]
+                (lambda (event)
+                  (interactive "e")
+                  (defvar last-point)
+                  (setq last-point (point))
+                  (goto-char (posn-point (event-start event)))
+                  (highlight-symbol-at-point)
+                  (goto-char last-point)))
+;;
+(global-set-key (kbd "M-n") 'highlight-symbol-next)
+(global-set-key (kbd "M-p") 'highlight-symbol-prev)
+
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+
+
+;;;;;;;;;;;;;;;;;;;
+;; UTF-8 EDITING ;;
+;;;;;;;;;;;;;;;;;;;
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; GENERAL PROGRAMMING SETTINGS ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-hook 'prog-mode-hook
+	  (lambda ()
+	    (setq show-trailing-whitespace 1)
+	    (text-scale-decrease 1)))
+(add-hook 'prog-mode-hook 'highlight-numbers-mode)
+(add-hook 'prog-mode-hook 'electric-pair-mode)
+(add-hook 'prog-mode-hook 'clean-aindent-mode)
+(add-hook 'prog-mode-hook 'hl-todo-mode)
+
+
+;;;;;;;;;;;;;;;;;;;;;
+;; C++ PROGRAMMING ;;
+;;;;;;;;;;;;;;;;;;;;;
+(defun my-c-common-mode-hook ()
+  (c-set-offset 'substatement-open 0)
+  (c-set-offset 'inline-open 0)
+  (c-set-offset 'comment-intro 0)
+  (c-set-offset 'defun-block-intro 'tab-width)
+  (c-set-offset 'statement-block-intro 'tab-width)
+  (c-set-offset 'substatement 'tab-width)
+  (c-set-offset 'topmost-intro 0)
+  (c-set-offset 'statement-cont 'tab-width)
+  (c-set-offset 'func-decl-cont 0)
+  (c-set-offset 'brace-list-open 0)
+  (c-set-offset 'brace-list-intro 'tab-width)
+  (c-set-offset 'brace-list-entry 0)
+  ;; EXPERIMENTAL:
+  ;;(c-set-offset 'arglist-intro 'c-lineup-arglist-intro-after-paren)
+  (c-set-offset 'arglist-intro 'tab-width)
+  )
+
+(add-hook 'c++-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode t)
+	    (setq tab-width 4)
+	    (setq c-basic-offset 4)
+	    )
+	  )
+(add-hook 'c++-mode-hook 'my-c-common-mode-hook)
+
+
+;;;;;;;;;;;;;;;;;;;;;;
+;; GLSL PROGRAMMING ;;
+;;;;;;;;;;;;;;;;;;;;;;
+(autoload 'glsl-mode "glsl-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.shd\\'" . glsl-mode))
+
+(defun my-glsl-mode-hook ()
+  "Custom OpenGL shader language(glsl) settings."
+
+  (setq indent-tabs-mode t)
+  (setq tab-width 4)
+  (c-set-offset 'defun-block-intro 4)
+  (c-set-offset 'substatement-open 0)
+  (c-set-offset 'statement-block-intro 4)
+  (c-set-offset 'knr-argdecl -100) ;; DIRTY HACK ??!
+  (c-set-offset 'arglist-intro 4)
+  (c-set-offset 'arglist-close 0)
+  )
+
+(add-hook 'glsl-mode-hook 'my-glsl-mode-hook)
+
+
+;;;;;;;;;;;;;;;;;;;;;
+;; SMART-TABS-MODE ;;
+;;;;;;;;;;;;;;;;;;;;;
+(smart-tabs-mode t)
+(smart-tabs-insinuate 'c++)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;
+;; WINDOW NAVIGATION ;;
+;;;;;;;;;;;;;;;;;;;;;;;
+(windmove-default-keybindings)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;
+;; BUFFER NAVIGATION ;;
+;;;;;;;;;;;;;;;;;;;;;;;
+(defun bufmove-default-keybindings (&optional modifier)
+  "Set up keybindings for switching between buffers in current window."
+  (interactive)
+  (unless modifier (setq modifier 'control))
+  (global-set-key (vector (list modifier 'left)) 'previous-buffer)
+  (global-set-key (vector (list modifier 'right)) 'next-buffer)
+  (global-set-key (kbd "C-<left>") 'previous-buffer)
+  (global-set-key (kbd "C-<right>") 'next-buffer)
+  )
+
+(bufmove-default-keybindings)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; KILL ACTIVE BUFFER ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; rebind C-x k to kill the active buffer instead of asking which one to kill
+(defun kill-default-buffer ()
+  "Kill the currently active buffer -- set to C-x k so that users are not asked
+which buffer they want to kill."
+  (interactive)
+  (let (kill-buffer-query-functions) (kill-buffer)))
+;;
+(global-set-key (kbd "C-x k") 'kill-default-buffer)
+
+
+;;;;;;;;;;;;;;;
+;; TERM-MODE ;;
+;;;;;;;;;;;;;;;
+(defun my-term-setup ()
+  (interactive)
+  (define-key term-raw-map (kbd "C-y") 'term-send-raw)
+  (define-key term-raw-map (kbd "C-p") 'term-send-raw)
+  (define-key term-raw-map (kbd "C-n") 'term-send-raw)
+  (define-key term-raw-map (kbd "C-s") 'term-send-raw)
+  (define-key term-raw-map (kbd "C-r") 'term-send-raw)
+  (define-key term-raw-map (kbd "M-w") 'kill-ring-save)
+  (define-key term-raw-map (kbd "M-y") 'helm-show-kill-ring)
+  (define-key term-raw-map (kbd "M-d") (lambda () (interactive) (term-send-raw-string "\ed")))
+  (define-key term-raw-map (kbd "<C-backspace>") (lambda () (interactive) (term-send-raw-string "\e\C-?")))
+  (define-key term-raw-map (kbd "M-p") (lambda () (interactive) (term-send-raw-string "\ep")))
+  (define-key term-raw-map (kbd "M-n") (lambda () (interactive) (term-send-raw-string "\en")))
+  (define-key term-raw-map (kbd "M-,") 'term-send-input)
+  (define-key term-raw-map (kbd "C-c y") 'term-paste)
+  (define-key term-raw-map (kbd "C-S-y") 'term-paste)
+  (define-key term-raw-map (kbd "C-h") nil) ; unbind C-h
+  (define-key term-raw-map (kbd "M-x") nil) ; unbind M-x
+  (define-key term-raw-map (kbd "C-c C-b") 'helm-mini)
+  (define-key term-raw-map (kbd "C-1") 'zygospore-toggle-delete-other-windows)
+  (define-key term-raw-map (kbd "C-2") 'split-window-below)
+  (define-key term-raw-map (kbd "C-3") 'split-window-right)
+  (define-key term-mode-map (kbd "C-0") 'delete-window))
+(add-hook 'term-mode-hook 'my-term-setup t)
+(setq term-buffer-maximum-size 0)
+;;
+(require 'term)
+;;
+;; taken from here: http://www.enigmacurry.com/2008/12/26/emacs-ansi-term-tricks/
+(defun visit-ansi-term ()
+  "If the current buffer is:
+     1) a running ansi-term named *ansi-term*, rename it.
+     2) a stopped ansi-term, kill it and create a new one.
+     3) a non ansi-term, go to an already running ansi-term
+        or start a new one while killing a defunt one"
+  (interactive)
+  (let ((is-term (string= "term-mode" major-mode))
+        (is-running (term-check-proc (buffer-name)))
+        (term-cmd "/bin/bash")
+        (anon-term (get-buffer "*ansi-term*")))
+    (if is-term
+        (if is-running
+            (if (string= "*ansi-term*" (buffer-name))
+                ;; (call-interactively 'rename-buffer)
+                (ansi-term term-cmd)
+              (if anon-term
+                  (switch-to-buffer "*ansi-term*")
+                (ansi-term term-cmd)))
+          (kill-buffer (buffer-name))
+          (ansi-term term-cmd))
+      (if anon-term
+          (if (term-check-proc "*ansi-term*")
+              (switch-to-buffer "*ansi-term*")
+            (kill-buffer "*ansi-term*")
+            (ansi-term term-cmd))
+        (ansi-term term-cmd)))))
+
+(global-set-key (kbd "<f1>") 'visit-ansi-term)
+
+
+;;;;;;;;;;;;;;;
+;; SHELL POP ;;
+;;;;;;;;;;;;;;;
+(require 'shell-pop)
+(global-set-key (kbd "<f2>") 'shell-pop)
+(setq shell-pop-window-size 25)
+
+
+;;;;;;;;;;;;;;;;;;
+;; BACKUP FILES ;;
+;;;;;;;;;;;;;;;;;;
+(defvar backup-directory "~/.backups")
+(if (not (file-exists-p backup-directory))
+    (make-directory backup-directory t))
+;;
+(setq
+ make-backup-files t    ; backup a file the first time it is saved
+ backup-directory-alist `((".*" . ,backup-directory)) ; save files in chosen backup folder
+ backup-by-copying t    ; copy the current file into backup directory
+ version-control t      ; version numbers for backup files
+ delete-old-versions t  ; delete unnecessary versions
+ kept-old-versions 6    ; oldest versions to keep when a new numbered backup is made (default: 2)
+ kept-new-versions 9    ; newest versions to keep when a new numbered backup is made (default: 2)
+ auto-save-default t    ; auto-save every buffer that visits a file
+ auto-save-timeout 40   ; number of seconds idle time before auto-save (default: 30)
+ auto-save-interval 400 ; number of keystrokes between auto-saves (default: 300)
+ )
+
+;;;;;;;;;;;;;;
+;; GDB-MODE ;;
+;;;;;;;;;;;;;;
+(setq-default gdb-many-windows t)
+
+
+;;;;;;;;;;;;;;;;;;;;
+;; CUSTOMIZATIONS ;;
+;;;;;;;;;;;;;;;;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (wombat)))
+ '(custom-safe-themes
+   (quote
+    ("8136cbb3b29b4c86ca3354d85005f527adcf9393b227980fc144a2c24ba63688" default)))
+ '(flymake-error-bitmap
+   (quote
+    (flymake-double-exclamation-mark modus-theme-fringe-red)))
+ '(flymake-note-bitmap (quote (exclamation-mark modus-theme-fringe-cyan)))
+ '(flymake-warning-bitmap (quote (exclamation-mark modus-theme-fringe-yellow)))
+ '(helm-completion-style (quote emacs))
+ '(hl-todo-keyword-faces
+   (quote
+    (("HOLD" . "#e5f040")
+     ("TODO" . "#feacd0")
+     ("NEXT" . "#b6a0ff")
+     ("THEM" . "#f78fe7")
+     ("PROG" . "#00d3d0")
+     ("OKAY" . "#4ae8fc")
+     ("DONT" . "#58dd13")
+     ("FAIL" . "#ff8059")
+     ("DONE" . "#44bc44")
+     ("NOTE" . "#f0ce43")
+     ("KLUDGE" . "#eecc00")
+     ("HACK" . "#eecc00")
+     ("TEMP" . "#ffcccc")
+     ("FIXME" . "#ff9977")
+     ("XXX+" . "#f4923b")
+     ("REVIEW" . "#6ae4b9")
+     ("DEPRECATED" . "#aaeeee"))))
+ '(ibuffer-deletion-face (quote dired-flagged))
+ '(ibuffer-filter-group-name-face (quote dired-mark))
+ '(ibuffer-marked-face (quote dired-marked))
+ '(ibuffer-title-face (quote dired-header))
+ '(package-selected-packages
+   (quote
+    (glsl-mode helm hl-todo highlight-symbol lua-mode clean-aindent-mode highlight-numbers shell-pop smart-tabs-mode undo-tree modus-vivendi-theme)))
+ '(shell-pop-shell-type
+   (quote
+    ("ansi-term" "*ansi-term*"
+     (lambda nil
+       (ansi-term shell-pop-term-shell)))))
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#ff8059")
+     (40 . "#feacd0")
+     (60 . "#f78fe7")
+     (80 . "#f4923b")
+     (100 . "#eecc00")
+     (120 . "#e5f040")
+     (140 . "#f8dec0")
+     (160 . "#bfebe0")
+     (180 . "#44bc44")
+     (200 . "#58dd13")
+     (220 . "#6ae4b9")
+     (240 . "#4ae8fc")
+     (260 . "#00d3d0")
+     (280 . "#c6eaff")
+     (300 . "#33beff")
+     (320 . "#72a4ff")
+     (340 . "#00baf4")
+     (360 . "#b6a0ff"))))
+ '(vc-annotate-very-old-color nil)
+ '(xterm-color-names
+   ["#000000" "#ff8059" "#44bc44" "#eecc00" "#33beff" "#feacd0" "#00d3d0" "#a8a8a8"])
+ '(xterm-color-names-bright
+   ["#181a20" "#f4923b" "#58dd13" "#e5f040" "#72a4ff" "#f78fe7" "#4ae8fc" "#ffffff"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:background "gray17")))))
